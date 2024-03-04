feb807d62fb93a4eaa55ba4f4d95fb1a37dc95b9
parthu143m <85431776+parthu143m@users.noreply.github.com>
2024-03-04 23:04:07 +0530

    Add files via upload

---
+++ parthu.js
@@ -0,0 +1,9 @@
+function change() {
+    var button = document.querySelector("button");
+    if (button.innerHTML === "DARK MODE") {
+        button.innerHTML = "WHITE MMODE";
+    }
+    else{
+        button.innerHTML = "DARK MODE"
+    }
+}
